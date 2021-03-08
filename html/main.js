d3a6a6903c0cea6278ac0b81f5509497ee444828
Tim Sutton <tim@kartoza.com>
2021-03-08 17:56:54 +0000

    Basic layers working

--- html/main.js
+++ html/main.js
@@ -3,14 +3,51 @@ window.onload = init();
 function init() {
     var map = new ol.Map({
         view: new ol.View({
-            center: [0, 0],
-            zoom: 7,
+            center: [-1543795.4, 4232854.9],
+            zoom: 5,
         }),
 
         layers: [
             new ol.layer.Tile({
-                source: new ol.source.OSM(),
+                title: "Boundaries",
+                extent: [-1543795.4, 4232854.9, -301891.2, 5312385.6],
+                source: new ol.source.TileWMS({
+                    url: "https://castelo.kartoza.com/mapproxy/service?",
+                    transition: 1,
+                    params: {
+                        'LAYERS': "Boundaries",
+                        'TILED': true
+                    },
+                })
+
+            }),
+            new ol.layer.Tile({
+                title: "OSM",
+                extent: [-867671.9, 4743686.0, -787701.3, 4813200.8],
+                source: new ol.source.TileWMS({
+                    url: "https://castelo.kartoza.com/mapproxy/service?",
+                    transition: 1,
+                    params: {
+                        'LAYERS': "OSM",
+                        'TILED': true
+                    },
+                })
+
+            }),
+            new ol.layer.Tile({
+                title: "Smallholding",
+                extent: [-822553.0, 4780579.4, -821704.8, 4781316.7],
+                source: new ol.source.TileWMS({
+                    url: "https://castelo.kartoza.com/mapproxy/service?",
+                    transition: 1,
+                    params: {
+                        'LAYERS': "Smallholding",
+                        'TILED': true
+                    },
+                })
+
             })
+
         ],
         target: 'ol-map',
     });
