ed062c4305cd4b10c1c1d4ea39dc4c78d6c677ad
meomancer <meomancer@gmail.com>
2021-09-29 14:35:19 +0700

    Update flows and add unicef theme

--- node_red/setup.sh
+++ node_red/setup.sh
@@ -5,3 +5,8 @@ npm install
 # For patches
 cp /data/patches/connection-parameters.js /data/node_modules/pg/lib/connection-parameters.js
 cd /usr/src/node-red
+
+# Patch node-red dashboard
+rm /data/node_modules/node-red-dashboard/dist/index.html
+ln -s /data/patches/node-red-dashboard/index.html /data/node_modules/node-red-dashboard/dist/index.html
+ln -s /data/patches/static /data/node_modules/node-red-dashboard/dist/unicef
