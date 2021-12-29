290f80f1af231f4cf392aa3631f44dd7c4fcff7b
Tim Sutton <tim@kartoza.com.com>
2021-12-29 10:04:16 +0000

    Fix sphinx warnings

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -58,7 +58,8 @@ html_theme = 'sizzle'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']   # Raises warning
+html_static_path = []  # see https://github.com/readthedocs/readthedocs.org/issues/1776#issuecomment-149700114
 
 # Theme specific options:
 # See https://docs.red-dove.com/sphinx_sizzle_theme/
